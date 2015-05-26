0339f4c963274224cc36bd44807dd685e1a06c64
Leo Wong <gnowoel@gmail.com>
2015-05-26 22:47:49 +0800

    Move parseBody() to lib/parse_body.js

--- test/parse_body.js
+++ test/parse_body.js
@@ -1,11 +1,12 @@
 var assert = require('chai').assert;
 var ydictClient = require('..');
 var fetchPage = require('../lib/fetch_page');
+var parseBody = require('../lib/parse_body');
 
 describe('parseBody', function() {
   it('should return parsed search result', function(done) {
     fetchPage('word', function(error, body) {
-      var result = ydictClient._private.parseBody(body);
+      var result = parseBody(body);
       assert.equal(result.word, 'word');
       assert.equal(result.pronunciation, 'KK[wɝd] DJ[wə:d]');
       done();
