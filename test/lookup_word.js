8ca6ee13e19ed5e0a466d44836646fc33b446b68
Leo Wong <gnowoel@gmail.com>
2015-05-28 22:22:21 +0800

    Add spelling auto-correct support

--- test/lookup_word.js
+++ test/lookup_word.js
@@ -2,11 +2,20 @@ var assert = require('chai').assert;
 var lookupWord = require('../lib/lookup_word');
 
 describe('lookupWord', function() {
-  it('should return definition of searched word', function(done) {
+  it('should return definition of a searched word', function(done) {
     var word = 'word';
     lookupWord(word, function(error, result) {
       assert.equal(result.word, 'word');
       assert.equal(result.pronunciation, 'KK[wɝd] DJ[wə:d]');
+      done();
+    });
+  });
+
+  it('should auto-correct spelling', function(done) {
+    var word = 'dictionar';
+    var corrected = 'dictionary';
+    lookupWord(word, function(error, result) {
+      assert.equal(result.suggestion, corrected);
       done();
     });
   });
