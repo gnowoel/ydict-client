d7632415d8e874723b623f611fe9258f5ba166f2
Leo Wong <gnowoel@gmail.com>
2015-05-29 23:42:26 +0800

    Highlighting keyword and its variants in example sentences

--- test/format_result.js
+++ test/format_result.js
@@ -8,6 +8,8 @@ describe('formatResult', function() {
     lookupWord(word, function(error, result) {
       var formatted = formatResult(word, result);
       assert.include(formatted, 'KK: [\u001b[1mwɝd\u001b[22m] DJ: [\u001b[1mwə:d\u001b[22m]');
+      assert.include(formatted, 'He threw in a \u001b[1mword\u001b[22m or two now and again.');
+      assert.include(formatted, 'I cannot remember the \u001b[1mwords\u001b[22m of the song.');
       done();
     });
   });
